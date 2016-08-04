85b708e7471d2195dcbafe3e6d2fc821958b95b4
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-08-05 04:20:24 +0900

    implement function to create new topic and comment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,25 @@
 class CommentsController < ApplicationController
+  
   def create
+    comment = Comment.new comment_params
+    comment.topic_id = params[:topic_id]
+    if comment.save
+      redirect_to root_path
+    else
+      render 'home/index'
+    end
+
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id)
   end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,25 @@
 class TopicsController < ApplicationController
   def create
+    topic = Topic.new(topic_params)
+    topic.user_id = current_user.id
+
+    if topic.save
+      redirect_to root_path
+    else
+      @topics = Topic.all
+      render 'home/index'
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:content)
   end
 end
