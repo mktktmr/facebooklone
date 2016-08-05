1c293f2442c3a79cde8f9e448ad45f04c7cae94f
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-08-06 05:57:42 +0900

    Update header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
-  def profile_img(user)
-    return image_tag(user.avatar, alt: user.name) if user.avatar?
+  def profile_img(user, options = {})
+    options.merge alt: user.name
+    return image_tag(user.avatar, options) if user.avatar?
 
     unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'no_image.png'
     end
-    image_tag(img_url, alt: user.name)
+    image_tag(img_url, options)
   end
 end
