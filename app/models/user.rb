cace8950ace1b38cfd207e9efada2178bce0ec27
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-08-06 01:06:34 +0900

    fixed bug user isn't created if sns account don't have email address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,51 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
 
   has_many :topics
   has_many :comments
+
+  def email_required?
+    super && provider.blank?
+  end
+  
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+
+    unless user
+      user = User.new(
+          name:     auth.extra.raw_info.name,
+          provider: auth.provider,
+          uid:      auth.uid,
+          email:    auth.info.email || '',
+          image_url:   auth.info.image,
+          password: Devise.friendly_token[0, 20]
+      )
+      # 確認メールの処理を飛ばす
+      # user.skip_confirmation! # TODO: deviseのconfirmableを利用する場合にコメントアウトを外す
+
+      user.save(validate: false)
+    end
+    user
+  end
+
+  def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+binding.pry
+    unless user
+      user = User.new(
+          name:     auth.info.nickname,
+          image_url: auth.info.image,
+          provider: auth.provider,
+          uid:      auth.uid,
+          email:    auth.info.email || '',
+          password: Devise.friendly_token[0, 20],
+      )
+      # user.skip_confirmation! # TODO: deviseのconfirmableを利用する場合にコメントアウトを外す
+      user.save
+    end
+    user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804172624) do
+ActiveRecord::Schema.define(version: 20160805155731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20160804172624) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.text     "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -46,9 +46,13 @@ ActiveRecord::Schema.define(version: 20160804172624) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "uid",                    default: "", null: false
+    t.string   "provider",               default: "", null: false
+    t.string   "image_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
 end
