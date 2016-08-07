00387254ceef50158af8243d2f46d9b9d02fe1cb
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-08-07 14:09:03 +0900

    Implemented function to notificete for sign up.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,19 @@
 Rails.application.configure do
+
+  # TODO: メーラーの本番環境設定
+  config.action_mailer.default_url_options = { host: '自分のHerokuアプリのドメイン' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
+   domain: "heroku.com",
+   address: "smtp.sendgrid.net",
+   port: 587,
+   authentication: :plain,
+   enable_starttls_auto: true
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
